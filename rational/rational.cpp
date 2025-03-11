f2eb180d0146994d64cd22305e00aeda8c7559fe
Seth Roberts <sar242004@gmail.com>
2025-03-10 20:12:47 -0500

    Save point

--- rational/rational.cpp
+++ rational/rational.cpp
@@ -1,1 +1,24 @@
 //Instructions at https://cs.harding.edu/gfoust/classes/comp3450/projects/rational
+include "rational.hpp"
+
+Rational::Rational() {
+	_num = 0;
+	_den = 1
+};
+
+Rational::Rational(int num, int den) {
+	if (den != 0) {
+		_num = num;
+		_den = den
+	}
+	else {
+		throw std::domain_error("Denominator cannot be 0");
+	}
+
+	Simplify(_num, _den);
+
+};
+
+Rational::Rational Simplify(int num, int den) {
+
+};
--- rational/rational.hpp
+++ rational/rational.hpp
@@ -1,1 +1,16 @@
 #pragma once
+class Rational {
+public:
+
+	Rational();
+	Rational(int num, int den);
+
+	int _num, _den;
+
+	Rational Simplify(int num, int den);
+
+private:
+
+
+
+};
